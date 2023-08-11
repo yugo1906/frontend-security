ed381feb04d11bec0c22b207666c98e2a5277c84
yugo <89300571+yugo1906@users.noreply.github.com>
2023-08-11 19:24:22 +0900

    update: 静的ファイルをサーバーから配信する

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res, next) => {
-  // 引数はレスポンスbodyにして、レスポンスを返す
-  res.end("Top Page: Hello World!");
+  res.end();
 });
 
 // サーバーを起動する
