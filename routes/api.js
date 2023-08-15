c15c002965a94658f1ab5fa5f383638bedee5b1b
yugo <89300571+yugo1906@users.noreply.github.com>
2023-08-15 19:40:15 +0900

    update: apiにget/postメソッドを追加

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send({ message: "Hello"});
+});
+
+router.use(express.json());
+router.post("/", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.end();
+})
+
 module.exports = router;
