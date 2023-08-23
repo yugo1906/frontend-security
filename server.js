1b70c6889cc3e57b4a45a105d82d6730b34b20cf
yugo <89300571+yugo1906@users.noreply.github.com>
2023-08-24 08:58:26 +0900

    update: csp検証用のページを作成

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const api = require("./routes/api");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/api", api);
 
 app.get("/", (req, res, next) => {
   res.end();
+});
+
+app.get("/csp", (req, res) => {
+  res.render("csp");
 });
 
 // サーバーを起動する
